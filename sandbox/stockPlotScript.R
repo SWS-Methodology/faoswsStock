8ad0888828c70a49f52635944b01b5450567ef34
rockclimber112358 <rockclimber112358@gmail.com>
2015-04-29 16:52:30 +0200

    Added plotting for wheat only, moved to sandbox

--- sandbox/stockPlotScript.R
+++ sandbox/stockPlotScript.R
@@ -28,6 +28,25 @@ for(windowLength in 1:20){
     coeff[[windowLength]]$windowLength = windowLength
 }
 
+windowLength = 15
+wheatItems = c("Wheat and products", "Maize and products",
+               "Cereals - Excluding Beer", "Cereals, Other",
+               "Barley and products")
+d[, pastStockSum := c(rep(NA, windowLength - 1),
+                      rollsum(Value, k = windowLength)),
+   by = ItemCode]
+## pastStockSum currently includes current line.  Must bump back one.
+d[, pastStockSum := c(NA, pastStockSum[-length(pastStockSum)]),
+   by = ItemCode]
+ggsave(paste0("Stock_cereal_summed_lag_", windowLength, ".png"),
+    ggplot(d[ItemName %in% wheatItems, ], aes(x = pastStockSum, y = Value)) +
+        geom_point() + facet_wrap( ~ ItemName, scale = "free") +
+        geom_smooth(method = "lm", formula = y ~ x + 0) +
+        labs(x = "Sum of previous 15 stock changes",
+             y = "Current stock change"),
+    width = 8, height = 8)
+
+
 coeff = do.call("rbind", coeff)
 setnames(coeff, "V1", "slope")
 ggsave("Slope_Distribution.png",
