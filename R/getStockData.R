10eff3615b6918d7ca4dec871dbea2a52de7aa5d
Bruno Vidigal <bruno.caetanovidigal@fao.org>
2016-03-03 12:41:49 +0100

    Added set up for other user

--- R/getStockData.R
+++ R/getStockData.R
@@ -13,6 +13,9 @@ getStockData = function(){
     if(exists("DEBUG_MODE") && DEBUG_MODE == FALSE){
         R_SWS_SHARE_PATH = Sys.getenv("R_SWS_SHARE_PATH")
         out = fread(paste0(R_SWS_SHARE_PATH, "/browningj/stock/usaStockVariabilityFAOSTAT.csv"))
+    ## If not on server, check for R_SWS_SHARE PATH
+    } else if(exists("R_SWS_SHARE_PATH")){
+        out = fread(paste0(R_SWS_SHARE_PATH, "/browningj/stock/usaStockVariabilityFAOSTAT.csv"))
     } else if(Sys.info()[7] == "josh"){
         out = fread("~/Documents/Github/faoswsStock/sandbox/Data/usaStockVariabilityFAOSTAT.csv")
     } else if(Sys.info()[7] == "rockc_000"){
